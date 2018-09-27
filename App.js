8c07d5d98bc8a9bc01f2c864691848b6d425dcab
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2018-09-27 17:43:36 -0300

    Copy AuthSession example from Expo docs

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { AuthSession } from 'expo'
+
+const FB_APP_ID = 'YOUR_APP_ID'
 
 export default class App extends React.Component {
+  state = {
+    result: null,
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button title="Open FB Auth" onPress={this._handlePressAsync} />
+        {this.state.result ? <Text>{JSON.stringify(this.state.result)}</Text> : null}
       </View>
-    );
+    )
+  }
+
+  _handlePressAsync = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl()
+    let result = await AuthSession.startAsync({
+      authUrl:
+        `https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
+        `&client_id=${FB_APP_ID}` +
+        `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+    })
+    this.setState({ result })
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
