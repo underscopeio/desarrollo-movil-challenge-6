3edcbc81d57cd5df673a29ee0cf4202f4631ffed
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2018-09-28 20:46:18 -0300

    Replace redirectUrl and texts

--- App.js
+++ App.js
@@ -12,18 +12,18 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Open FB Auth" onPress={this._handlePressAsync} />
+        <Button title="Open Spotify Auth" onPress={this._handlePressAsync} />
         {this.state.result ? <Text>{JSON.stringify(this.state.result)}</Text> : null}
       </View>
     )
   }
 
   _handlePressAsync = async () => {
-    let redirectUrl = AuthSession.getRedirectUrl()
-    let result = await AuthSession.startAsync({
+    const redirectUrl = AuthSession.getRedirectUrl()
+    const result = await AuthSession.startAsync({
       authUrl:
-        `https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
-        `&client_id=${FB_APP_ID}` +
+        `https://accounts.spotify.com/authorize?response_type=token` +
+        `&client_id=${SPOTIFY_CLIENT_ID}` +
         `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
     })
     this.setState({ result })
